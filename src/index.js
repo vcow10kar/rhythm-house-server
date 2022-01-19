16af3e5354422e068406d0f419b00c0ccd227817
vcow10kar <vkawthankar@gmail.com>
2022-01-19 18:19:21 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors({origin: `${process.env.FRONTEND_URL}`, credentials: true}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
